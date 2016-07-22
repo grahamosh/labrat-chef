38b68b1f7648541ad1d8cf6e6250e5540aeedb6f
root <root@grahamosh6.mylabserver.com>
2016-07-22 18:14:17 +0000

    Initial Commit

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["grahamosh1"] = { "site_title" => "Web 1 coming soon", "port" => 80, "domain" => "grahamosh1.mylabserver.com" }
+default["apache"]["sites"]["grahamosh1b"] = { "site_title" => "Web 1b coming soon", "port" => 80, "domain" => "grahamosh1b.mylabserver.com" }
